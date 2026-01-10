6940b09bd12382d8d598e31a609108fb4cba9708
Nam Pham <pvnam95@hotmail.com>
2026-01-10 16:50:18 +0900

    fix: update remoteit connection script and add Ansible playbook for deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-# add localhost to known_hosts to avoid prompt
-ssh-keyscan -H localhost >> ~/.ssh/known_hosts
-
-# test ssh connection localhost:30001 with username/password pi/zzzz
-sshpass -p "zzzz" ssh -o StrictHostKeyChecking=no -p 30001 pi@localhost "echo 'SSH connection to localhost:30001 successful'"
+# Run the Ansible playbook
+ansible-playbook playbook.yml
